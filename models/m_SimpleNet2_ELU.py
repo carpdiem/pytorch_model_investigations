ad90fad2bf82a623f9568dc820cc9dfa55a7d34d
Michael Woods <mail@michaelwoods.us>
2024-05-08 22:16:17 -0700

    added SimpleNet and a bunch of SimpleNet2 variants to test

---
+++ models/m_SimpleNet2_ELU.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class DeepNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer1 = nn.Linear(28*28, 10*10)
+        self.activation = nn.ELU(alpha=1.0)
+        self.layer2 = nn.Linear(10*10, 10)
+        
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.activation(x)
+        x = self.layer2(x)
+        return x
