5b4bac29a76b188cb6d84d155b3f2356955012ef
Michael Woods <mail@michaelwoods.us>
2024-06-06 16:19:27 -0700

    added a batch of different depth MLPs for testing with different activation functions

---
+++ models/m_SimpleNet2_10layers_tanh.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class DeepNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.num_middle_layers = 10
+        self.layer1 = nn.Linear(28*28, 10*10)
+        self.middle_layers = [nn.Linear(10*10, 10*10) for i in range(self.num_middle_layers)]
+        self.activations = [nn.Tanh() for i in range(self.num_middle_layers)]
+        self.layerLast = nn.Linear(10*10, 10)
+        
+
+    def forward(self, x):
+        x = self.layer1(x)
+        for middle_layer, activation in zip(self.middle_layers, self.activations):
+            x = middle_layer(x)
+            x = activation(x)
+        x = self.layerLast(x)
+        return x
---
+++ models/m_SimpleNet2_1layers.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class DeepNet(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.num_middle_layers = 1
+        self.layer1 = nn.Linear(28*28, 10*10)
+        self.middle_layers = [nn.Linear(10*10, 10*10) for i in range(self.num_middle_layers)]
+        self.layerLast = nn.Linear(10*10, 10)
+        
+
+    def forward(self, x):
+        x = self.layer1(x)
+        for middle_layer in self.middle_layers:
+            x = middle_layer(x)
+        x = self.layerLast(x)
+        return x
