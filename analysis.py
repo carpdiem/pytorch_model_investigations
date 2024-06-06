305dc0b1a038d5f930f55c1a404fbdd90e12736c
Michael Woods <mail@michaelwoods.us>
2024-06-06 16:18:43 -0700

    added svg processor and some formatting niceties

--- analysis.py
+++ analysis.py
@@ -1,7 +1,22 @@
 import matplotlib.pyplot as plt
+import matplotlib.style as mplstyle
 from matplotlib.ticker import AutoMinorLocator
+from svg_processor import process_svg
 import numpy as np
 import os
+
+def get_model_names_with_filters(path, pos_filters, neg_filters = []):
+    file_list = os.listdir(path)
+
+    for substring in pos_filters:
+        file_list = [file for file in file_list if substring in file]
+
+    for substring in neg_filters:
+        file_list = [file for file in file_list if substring not in file]
+
+    model_names = [os.path.splitext(file)[0] for file in file_list]
+
+    return model_names
 
 def get_model_names_with_prefix(path, prefix):
     file_list = os.listdir(path)
@@ -24,61 +39,83 @@ def load_training_results(*models, path='models/results'):
     
     return results
 
-def simple_graph(results, show=True, path=None):
-    plt.figure(figsize=(6,4))
-    ax = plt.gca()
+def simple_graph(results, show=True, path=None, filetype=None):
+    def plot_it(style, filename, only_show = False):
+        with mplstyle.context(style):
+            plt.rcParams['font.family'] = 'monospace'
+            plt.rcParams['font.monospace'] = 'Iosevka Fixed'
+            plt.figure(figsize=(6,4))
+            ax = plt.gca()
+        
+            def forward(x):
+                return -np.log2(1-x)
+            
+            def inverse(x):
+                return 1-2**(-x)
+            
+            ax.set_yscale('function', functions=(forward, inverse))
+            #breakpoint()
+            for modelname, data in sorted(results.items()):
+                ax.plot(data['FLOPs'], data['Accuracy'], label = modelname)
+            
+            plt.legend()
+            plt.xlabel('FLOPs')
+            plt.ylabel('Accuracy')
+            plt.title('FLOPs vs Accuracy')
+        
+        
+            # Do some trickery to make the "funny log" 1-2^x scale work
+            y_min, y_max = ax.get_ylim()
+            
+            high_tick_exponent = np.ceil(np.log2(1 - y_max))
+            low_tick_exponent = np.floor(np.log2(1 - y_min))
+        
+            y_ticks = np.arange(low_tick_exponent, high_tick_exponent - 1, -1)
+            y_ticks = 1. - 2**y_ticks
+        #    y_ticks = np.insert(y_ticks, 0, y_min)
+        #    y_ticks = np.append(y_ticks, y_max)
+        
+            ax.set_yticks(y_ticks)
+            ax.tick_params(which='major', length=4)
+        
+            # Set some minor ticks for the "funny log" scale to make it clear what's going on
+            minor_ticks = []
+            num_minor_ticks = 8
+            for i in range(len(y_ticks) - 1):
+                start = y_ticks[i]
+                end = y_ticks[i+1]
+                minor_ticks.extend(np.linspace(start, end, num_minor_ticks + 1)[1:])
+            ax.set_yticks(minor_ticks, minor=True)
+            ax.tick_params(which='minor', length=2, color='gray')
+        
+            plt.tight_layout()
 
-    def forward(x):
-        return -np.log2(1-x)
-    
-    def inverse(x):
-        return 1-2**(-x)
-    
-    ax.set_yscale('function', functions=(forward, inverse))
+            if only_show:
+                plt.show()
 
-    for modelname, data in results.items():
-        ax.plot(data['FLOPs'], data['Accuracy'], label = modelname)
-    
-    plt.legend()
-    plt.xlabel('FLOPs')
-    plt.ylabel('Accuracy')
-    plt.title('FLOPs vs Accuracy')
+            else:
+                plt.savefig(filename)
 
-
-    # Do some trickery to make the "funny log" 1-2^x scale work
-    y_min, y_max = ax.get_ylim()
-    
-    high_tick_exponent = np.ceil(np.log2(1 - y_max))
-    low_tick_exponent = np.floor(np.log2(1 - y_min))
-
-    y_ticks = np.arange(low_tick_exponent, high_tick_exponent - 1, -1)
-    y_ticks = 1. - 2**y_ticks
-    y_ticks = np.insert(y_ticks, 0, y_min)
-    y_ticks = np.append(y_ticks, y_max)
-
-    ax.set_yticks(y_ticks)
-    ax.tick_params(which='major', length=4)
-
-    # Set some minor ticks for the "funny log" scale to make it clear what's going on
-    minor_ticks = []
-    num_minor_ticks = 8
-    for i in range(len(y_ticks) - 1):
-        start = y_ticks[i]
-        end = y_ticks[i+1]
-        minor_ticks.extend(np.linspace(start, end, num_minor_ticks + 1)[1:])
-    ax.set_yticks(minor_ticks, minor=True)
-    ax.tick_params(which='minor', length=2, color='gray')
-
-    plt.tight_layout()
+            plt.close()
     
     if path is not None:
-        plt.savefig(path)
+        if filetype is not None:
+            filenames = [path + "_dark." + filetype, path + "_light." + filetype]
+        else:
+            filenames = [path + "_dark.png", path + "_light.png"]
+
+        styles = ['gruvbox-dark.mplstyle', 'gruvbox-light.mplstyle']
+
+        for style, filename in zip(styles, filenames):
+            plot_it(style, filename)
+            if filetype == 'svg':
+                process_svg(filename)
     
     if show:
-        plt.show()
+        plot_it('gruvbox-dark.mplstyle', _, only_show=True)
 
 
-def try_me(model_prefix = 'm_SimpleNet2'):
-    model_names = get_model_names_with_prefix('models/results', model_prefix)
+def try_me(pos_filters = ['m_SimpleNet2'], neg_filters = [], show=True, path=None, filetype=None):
+    model_names = get_model_names_with_filters('models/results', pos_filters, neg_filters)
     results = load_training_results(*model_names)
-    simple_graph(results)
+    simple_graph(results, show=show, path=path, filetype=filetype)
---
+++ svg_processor.py
@@ -0,0 +1,63 @@
+
+import xml.etree.ElementTree as ET
+import random
+import string
+
+def generate_unique_id():
+    """Generate a short random hash for IDs."""
+    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=6))
+
+def extract_stroke_color(style):
+    """Extracts the stroke color from a style attribute string."""
+    if 'stroke:' in style:
+        parts = style.split(';')
+        stroke_entry = [part for part in parts if 'stroke:' in part]
+        if stroke_entry:
+            return stroke_entry[0].split(':')[1].strip()
+    return None
+
+def process_svg(svg_path):
+    ns = {'svg': 'http://www.w3.org/2000/svg'}
+    ET.register_namespace('', ns['svg'])
+    tree = ET.parse(svg_path)
+    root = tree.getroot()
+
+    axes_group = root.find(".//*[@id='axes_1']", namespaces=ns)
+    legend_group = root.find(".//*[@id='legend_1']", namespaces=ns)
+
+    if not axes_group or not legend_group:
+        raise ValueError("SVG does not contain the required 'axes_1' or 'legend_1' groups")
+    
+    legend_items = list(legend_group)[1:]  # Skip the first element if it's a background patch
+    for i in range(0, len(legend_items), 2):
+        if i+1 >= len(legend_items):
+            continue
+        sample_g = legend_items[i]
+        label_g = legend_items[i+1]
+        
+        bounding_g = ET.SubElement(legend_group, 'g', {'id': generate_unique_id(), 'class': 'legend_item'})
+        legend_group.remove(sample_g)
+        legend_group.remove(label_g)
+        bounding_g.append(sample_g)
+        bounding_g.append(label_g)
+    
+    for bounding_g in legend_group.findall('g', namespaces=ns):
+        sample_g = bounding_g[0]
+        path = sample_g.find('.//svg:path', namespaces=ns)
+        if path is None:
+            continue
+        style = path.get('style')
+        if style:
+            stroke_color = extract_stroke_color(style)
+            if stroke_color:
+                for child in axes_group:
+                    path = child.find('.//svg:path', namespaces=ns)
+                    if path is not None and extract_stroke_color(path.get('style')) == stroke_color:
+                        child.set('id', bounding_g.get('id'))
+                        child.set('class', 'plot_line')
+                        break
+
+    tree.write('p_' + svg_path)
+
+# Example usage
+# process_svg('example.svg')
