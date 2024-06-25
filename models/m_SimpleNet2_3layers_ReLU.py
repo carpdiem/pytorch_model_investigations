3a35b84e287e97d5d55f39c2da1d0bf6e591f4d6
Michael Woods <mail@michaelwoods.us>
2024-06-25 16:46:57 -0700

    fixed depth of architecture investigation models to apply activation function on inital layer as well

--- models/m_SimpleNet2_3layers_ReLU.py
+++ models/m_SimpleNet2_3layers_ReLU.py
@@ -1,18 +1,22 @@
 import torch
 import torch.nn as nn
 
+default_activation = lambda : nn.ReLU()
+
 class DeepNet(nn.Module):
     def __init__(self):
         super().__init__()
+        self.initial_activation = default_activation()
         self.num_middle_layers = 3
         self.layer1 = nn.Linear(28*28, 10*10)
         self.middle_layers = nn.ModuleList([nn.Linear(10*10, 10*10) for i in range(self.num_middle_layers)])
-        self.activations = nn.ModuleList([nn.ReLU() for i in range(self.num_middle_layers)])
+        self.activations = nn.ModuleList(default_activation() for i in range(self.num_middle_layers)])
         self.layerLast = nn.Linear(10*10, 10)
         
 
     def forward(self, x):
         x = self.layer1(x)
+        x = self.initial_activation(x)
         for middle_layer, activation in zip(self.middle_layers, self.activations):
             x = middle_layer(x)
             x = activation(x)
